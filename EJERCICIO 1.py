cba0199de4b76e551923537d6534edd18dc03e30
ybermudcas <ybermudcas@educacion.navarra.es>
2023-11-27 20:23:03 +0100

    EJERCICIO 1

---
+++ EJERCICIO 1.py
@@ -0,0 +1,25 @@
+lista_de_nombres = []
+
+def ListaNombres(nombre, apellido):
+    """Parámetros:
+    lista_de_nombre: esta lista guarda una cantidad indeterminada de nombres en este formato:
+    ["Nombre Apellido 1", "Nombre Apellido 2",..., "Nombre Apellido N"]
+    Salidas:
+    La función retorna la lista de nombres ordenados alfabéticamente en este formato:
+    ["Apellido, Nombre 1", "Apellido, Nombre 2",..., "Apellido, Nombre N"]
+    """
+    for i in range(len(nombre, apellido)):
+        lista_de_nombres.append(apellido, nombre)
+    return lista_de_nombres
+
+def OrdenarNombre(nombre, apellido):
+    """Esta función recibe un "nombre y apellido" y devuelve el "apellido, nombre".
+    -Parámetros:
+        - Nombre en el formato "Nombre Apellido"
+    - Salida:
+        - Nombre en el formato "Apellido, Nombre"
+    """     
+    return apellido, nombre
+
+
+
